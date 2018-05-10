83f17b3e381e7a0cf494d0d1a7fa4dcf41f1e7c9
Ada Joule <ada.fulmina@gmail.com>
2018-05-11 01:34:24 +0700

    Add preprocessing

--- yugioh_model.py
+++ yugioh_model.py
@@ -1,6 +1,7 @@
 import keras.backend as K
 from keras.models import Sequential
 from keras.layers import Embedding, Dense, Conv1D, MaxPooling1D, GlobalAveragePooling1D
+from keras.preprocessing.sequence import pad_sequences
 
 import numpy as np
 
@@ -8,7 +9,7 @@ import characters
 
 def get_model(name_length=100, classes=3):
     model = Sequential()
-    model.add(Embedding(len(characters.chars), 32))
+    model.add(Embedding(len(characters.chars) + 1, 32))
     model.add(Conv1D(64, 3, activation='relu'))
     model.add(Conv1D(64, 3, activation='relu'))
     model.add(MaxPooling1D(3))
@@ -24,7 +25,14 @@ def get_model(name_length=100, classes=3):
     return model
 
 def str2arr(s):
-    return np.fromiter(map(characters.char2ind, s.lower()), float)
+    return list(map(characters.char2ind, s.lower()))
+
+def preprocess_card_names(card_names, name_length=100):
+    return np.array(pad_sequences(
+               list(map(str2arr, card_names)),
+               maxlen=name_length,
+               padding='post',
+               truncating='post'))
 
 def predict(model, card_names):
     model.predict()
