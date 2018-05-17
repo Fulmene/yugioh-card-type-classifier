ad0b3d1db91aaacdc5dcae8796c99e75a7124db4
Ada Joule <ada.fulmina@gmail.com>
2018-05-17 22:40:39 +0700

    Add graph plotting

--- train_model.py
+++ train_model.py
@@ -1,4 +1,5 @@
 from keras.callbacks import ModelCheckpoint
+import matplotlib.pyplot as plt
 
 import yugioh_data
 import yugioh_model
@@ -13,8 +14,26 @@ model = yugioh_model.get_model()
 
 weights_path = 'model/weights.h5'
 
-model.fit(x=train_x, y=train_y, batch_size=32, epochs=100, verbose=2,
+history = model.fit(x=train_x, y=train_y, batch_size=32, epochs=80, verbose=1,
         callbacks=[ModelCheckpoint(weights_path)],
         validation_data=(val_x, val_y))
+
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.savefig('acc.png', bbox_inches='tight')
+plt.clf()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.savefig('loss.png', bbox_inches='tight')
+plt.clf()
 
 print(model.test_on_batch(test_x, test_y))
