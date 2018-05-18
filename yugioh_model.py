df8539875a162b1d8246f63e00631397e15e5e89
Ada Joule <ada.fulmina@gmail.com>
2018-05-18 14:25:18 +0700

    Use functional API for model

--- yugioh_model.py
+++ yugioh_model.py
@@ -1,6 +1,6 @@
 import keras.backend as K
-from keras.models import Sequential
-from keras.layers import Embedding, Dense, Dropout, Conv1D, MaxPooling1D, Flatten
+from keras.models import Model
+from keras.layers import Input, Embedding, Dense, Dropout, Conv1D, MaxPooling1D, Flatten
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
 
@@ -9,22 +9,23 @@ import numpy as np
 import yugioh_data
 
 def get_model(name_length=60, classes=3):
-    model = Sequential()
-    model.add(Embedding(len(yugioh_data.chars) + 1, 32, input_length=name_length))
-    model.add(Dropout(0.2))
-    model.add(Conv1D(256, 5, activation='relu'))
-    model.add(Conv1D(256, 5, activation='relu'))
-    model.add(MaxPooling1D())
-    model.add(Dropout(0.5))
-    model.add(Conv1D(512, 5, activation='relu'))
-    model.add(Conv1D(512, 5, activation='relu'))
-    model.add(MaxPooling1D())
-    model.add(Dropout(0.5))
-    model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(classes, activation='softmax'))
-    
+    inputs = Input(shape=(name_length,))
+    embedding = Embedding(len(yugioh_data.chars) + 1, 32)(inputs)
+    dropout1 = Dropout(0.2)(embedding)
+    conv1 = Conv1D(256, 5, activation='relu')(dropout1)
+    conv2 = Conv1D(256, 5, activation='relu')(conv1)
+    maxpooling1 = MaxPooling1D()(conv2)
+    dropout2 = Dropout(0.5)(maxpooling1)
+    conv3 = Conv1D(512, 5, activation='relu')(dropout2)
+    conv4 = Conv1D(512, 5, activation='relu')(conv3)
+    maxpooling2 = MaxPooling1D()(conv4)
+    dropout3 = Dropout(0.5)(maxpooling2)
+    flatten = Flatten()(dropout3)
+    dense = Dense(512, activation='relu')(flatten)
+    dropout4 = Dropout(0.5)(dense)
+    outputs = Dense(classes, activation='softmax')(dropout4)
+
+    model = Model(inputs=inputs, outputs=outputs)
     model.compile(loss='categorical_crossentropy',
         optimizer='adam',
         metrics=['accuracy'])
